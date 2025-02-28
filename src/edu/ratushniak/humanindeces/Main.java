640e4f5449fdccf1e0f5ecf56b6aa6d94fb44357
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-02-28 10:11:37 +0200

    Refactor Main.java

--- src/edu/ratushniak/humanindeces/Main.java
+++ src/edu/ratushniak/humanindeces/Main.java
@@ -1,8 +1,13 @@
 package edu.ratushniak.humanindeces;
 
 public class Main {
+
     public static void main(String[] args) {
-        var humanIMB = new HumanIMB(80, 1.52);
+        double weight = 80.0;
+        double imb = 1.52;
+
+        var humanIMB = new HumanIMB(weight, imb);
+
         System.out.println(humanIMB.Result());
     }
 }
