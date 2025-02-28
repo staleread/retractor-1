fd9ba66d174a89910ab3009e406fd436572a6097
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-02-28 10:00:15 +0200

    Use var in obvious situations

--- src/edu/ratushniak/humanindeces/Main.java
+++ src/edu/ratushniak/humanindeces/Main.java
@@ -2,7 +2,7 @@ package edu.ratushniak.humanindeces;
 
 public class Main {
     public static void main(String[] args) {
-        HumanIMB humanIMB = new HumanIMB(80,1.52);
+        var humanIMB = new HumanIMB(80, 1.52);
         System.out.println(humanIMB.Result());
     }
 }
